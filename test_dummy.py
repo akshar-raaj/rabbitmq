b85205a53b69e2d7256924d2ede64be6f8fa30e4
Akshar Raaj <akshar.r@extremecompute.com>
2025-04-12 23:59:39 +0530

    Added few tests. A test class with several methods. A test method using Pytest parametrize feature.

--- test_dummy.py
+++ test_dummy.py
@@ -1,2 +1,18 @@
+import pytest
+
+
 def test_dummy():
     assert 1 == 1
+
+
+class TestDummy(object):
+
+    def test_add(self):
+        assert 1 + 2 == 3
+
+    def test_sub(self):
+        assert 1 - 2 == -1
+
+    @pytest.mark.parametrize("a, b, expected", [(1, 2, 2), (2, 3, 6)])
+    def test_mul(self, a, b, expected):
+        assert a * b == expected
