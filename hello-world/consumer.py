b129643a001b4fff85b20851c0f31e814c6ad416
Akshar Raaj <akshar.r@extremecompute.com>
2025-04-14 07:50:43 +0530

    Sane defaults. Better modularity and importability. Allow ability to run as a module as well as a script.

--- hello-world/consumer.py
+++ hello-world/consumer.py
@@ -1,23 +1,32 @@
 import pika
 import sys
 
+DEFAULT_QUEUE_NAME = 'hello'
+
 
 def on_message_callback(channel, method, properties, body):
-    print(f"Received {body.decode('utf-8')}")
+    print(f"Received: {body.decode('utf-8')}")
     channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
-def main():
-    connection = pika.BlockingConnection()
+def get_connection(host='localhost', port=5672):
+    parameters = pika.ConnectionParameters(host=host, port=port)
+    connection = pika.BlockingConnection(parameters)
+    return connection
+
+
+def consume(queue):
+    connection = get_connection()
     channel = connection.channel()
-    channel.basic_consume('hello', on_message_callback=on_message_callback, auto_ack=False)
-    channel.basic_consume('namaste', on_message_callback=on_message_callback, auto_ack=False)
+    # Do not auto acknowledge messages. Messages should be acknowledged manually.
+    channel.basic_consume(queue, on_message_callback=on_message_callback, auto_ack=False)
     channel.start_consuming()
 
 
 if __name__ == '__main__':
+    queue = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_QUEUE_NAME
     try:
-        main()
+        consume(queue=queue)
     except KeyboardInterrupt:
         print("Interrupting")
         sys.exit(0)
